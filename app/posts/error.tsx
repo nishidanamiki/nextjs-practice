d9d4cddd1ae9d85a89c12fbf988ee8f0b9e4b4c6
nishida namiki <ymdnmk1201@gmail.com>
2026-06-30 17:07:21 +0900

    feat: PostsページにLoading UIとError UIを追加

---
+++ app/posts/error.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useEffect } from "react";
+
+interface ErrorProps {
+  error: Error;
+  reset: () => void;
+}
+
+export default function Error({ error, reset }: ErrorProps) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <p>{error.message}</p>
+      <button
+        onClick={() => {
+          reset();
+        }}
+      >
+        Try again
+      </button>
+    </div>
+  );
+}
---
+++ app/posts/loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading() {
+  return <p>投稿を読み込んでいます...</p>;
+}
